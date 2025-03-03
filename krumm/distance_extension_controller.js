6e68fcdf0f0ce7575e382397c51b97b0ba4eef1e
Max Power <kevin.melchert@gmail.com>
2025-03-03 22:38:51 +0100

    use intl

--- krumm/distance_extension_controller.js
+++ krumm/distance_extension_controller.js
@@ -24,20 +24,24 @@ application.register("krumm", class extends Stimulus.Controller {
     const originalDistance  = parseFloat(this.originalDistanceTarget.value)  || 0
     const prolongedDistance = parseFloat(this.prolongedDistanceTarget.value) || 0
 
-    const checker = new DistanceExtensionChecker(originalDistance, prolongedDistance)
+    const checker   = new DistanceExtensionChecker(originalDistance, prolongedDistance)
+    const formatter = new Intl.NumberFormat('de-DE', {
+      style: 'unit',
+      unit: 'kilometer',
+      unitDisplay: 'short', // Can be 'short', 'long', or 'narrow'
+      minimumFractionDigits: 1,
+      maximumFractionDigits: 2
+    });
     
-    this.originalDistanceOutputTarget.textContent  = this.formatResult(checker.originalDistance)
-    this.prolongedDistanceOutputTarget.textContent = this.formatResult(checker.prolongedDistance)
-    this.maxProlongedDistanceTarget.textContent    = this.formatResult(checker.getMaxProlongedDistance())
-    this.maxDeviationTarget.textContent            = this.formatResult(checker.getMaxExtension())
-    this.differenceTarget.textContent              = this.formatResult(Math.abs(checker.getDifference())) + " " + (checker.getDifference() > 0 ? "drüber" : "drunter")
+    
+    this.originalDistanceOutputTarget.textContent  = formatter.format(checker.originalDistance)
+    this.prolongedDistanceOutputTarget.textContent = formatter.format(checker.prolongedDistance)
+    this.maxProlongedDistanceTarget.textContent    = formatter.format(checker.getMaxProlongedDistance())
+    this.maxDeviationTarget.textContent            = formatter.format(checker.getMaxExtension())
+    this.differenceTarget.textContent              = formatter.format(Math.abs(checker.getDifference())) + " " + (checker.getDifference() > 0 ? "drüber" : "drunter")
     
     this.resultTarget.textContent = checker.isValid() ? 'Nein!' : 'Ja!'
     
     document.body.classList.toggle('krumm', !checker.isValid())
-  }
-  
-  formatResult(number) {
-    return number.toFixed(2).concat(' km')
   }
 })
